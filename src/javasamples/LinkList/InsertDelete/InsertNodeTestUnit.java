37ab2e4c078164c901554417c4cfdd09115b90f4
jhoseyniv <jhoseyniv@gmail.com>
2021-12-04 13:43:52 +0330

    unittest

---
+++ src/javasamples/LinkList/InsertDelete/InsertNodeTestUnit.java
@@ -0,0 +1,16 @@
+package javasamples.LinkList.InsertDelete;
+
+import  static  org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class InsertNodeTestUnit {
+        InsertNode insertNode= new InsertNode();
+
+        @Test
+       public void insertAndDeleteTest(){
+
+        }
+
+
+}
--- src/javasamples/LinkList/LeftRotation.java
+++ src/javasamples/LinkList/LeftRotation.java
@@ -1,10 +1,12 @@
 package javasamples.LinkList;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.*;
 
 public class LeftRotation {
 
-    public static List<Integer> rotateRight(int d, List<Integer> arr,int direction) {
+    public static List<Integer> rotatLeft(int d, List<Integer> arr) {
         // Write your code here
         List linkedList = new LinkedList();
         for(int j=0;j<arr.size();j++){
@@ -19,7 +21,7 @@ public class LeftRotation {
         return linkedList;
     }
 
-    public static List<Integer> rotateLeft(int d, List<Integer> arr,int direction) {
+    public static List<Integer> rotateRight(int d, List<Integer> arr) {
         // Write your code here
         List linkedList = new LinkedList();
         for(int j=0;j<arr.size();j++){
@@ -36,7 +38,7 @@ public class LeftRotation {
 
     public static void main(String args[]){
         List<Integer> arrLeft=new ArrayList<Integer>(){
-            {add(1);add(2);add(3);add(4);add(5);}
+            {add(1);add(2);add(3);add(4);add(5);add(6);add(7);add(8);add(9);add(10);}
         };
 //         System.out.print("original array = ");
 //        for(int i=0;i<arrLeft.size();i++)
@@ -57,11 +59,14 @@ public class LeftRotation {
         for(int i=0;i<arrRigth.size();i++)
             System.out.print(arrRigth.get(i) +" , ");
 
-        arrRigth =rotateLeft(3,arrRigth,0);
+        arrRigth =rotatLeft(3,arrRigth);
 
         System.out.println("Shifted array = ");
         for(int i=0;i<arrRigth.size();i++)
             System.out.print(arrRigth.get(i) +" , ");
 
     }
+
+
+
 }
