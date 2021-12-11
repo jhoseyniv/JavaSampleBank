08885fa77ceffe10c676a5e290e8f74c26222429
jhoseyniv <jhoseyniv@gmail.com>
2021-12-11 12:43:57 +0330

    generic

---
+++ src/javasamples/Fibonachi/TimeComplexity.java
@@ -0,0 +1,28 @@
+package javasamples.Fibonachi;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TimeComplexity {
+    public static void main(String[] args){
+
+        //time for fibo recuresive
+        long start1= new Date().getTime();
+        FibonacciNumbers.fiboRecurseive(50);
+        long end1= new Date().getTime();
+        long difference = end1 - start1;
+        System.out.println(difference + " mili seconds.");
+
+        //time for fibo dynamic programming
+        long start2= new Date().getTime();
+        FibonacciNumbers.fiboDP(150000);
+        long end2= new Date().getTime();
+        long difference2 = end2 - start2;
+
+        System.out.println(difference2 + " mili seconds.");
+
+
+
+    }
+}
